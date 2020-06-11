117cbde7cac06e93c634f1f71af9725fe900d3c7
sashaFPV <andrei.cazanov2017@gmail.com>
2020-06-11 18:35:00 +0300

    Исходная версия

---
+++ SmartTest 2/PCH.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <map>
+#include <memory>
+#include <algorithm>
+#include <iterator>
---
+++ SmartTest 2/SmartTest 2.cpp
@@ -0,0 +1,7 @@
+﻿#include <iostream>
+
+int main()
+{
+
+    std::cout << "\n> Programm finished (0_0) " << std::endl;
+}
